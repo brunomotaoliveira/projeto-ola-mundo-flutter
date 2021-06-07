66e8fecfa292b9798f7dab8d9435fb8b3dc78419
brunomotadev <brunomotadev@gmail.com>
2021-06-07 12:07:12 -0300

    estados(statelessWidget)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-      child: Center(
-          child: Text(
-    'Bruno Mota',
-    textDirection: TextDirection.ltr,
-    style: TextStyle(color: Colors.white, fontSize: 50.0),
-  ))));
+  runApp(AppWidget(
+    title: 'Bruno Mota',
+  ));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return (Container(
+        child: Center(
+            child: Text(
+      title,
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.white, fontSize: 50.0),
+    ))));
+  }
 }
